8d29dbe0e5ae8695e85d3487d5d212ff71a2d84d
simgee <simgeekiz48@gmail.com>
2016-04-07 10:31:44 +0300

    minor change

--- Bulmaca/settings.py
+++ Bulmaca/settings.py
@@ -124,7 +124,8 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "Bulmaca/static"),
+    os.path.join(BASE_DIR, "static"),
+	'Bulmaca/static'
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")

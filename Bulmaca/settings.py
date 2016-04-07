09bd9e8d4e1d2fd98a3d7a8b6eab605b5056c4d6
simgee <simgeekiz48@gmail.com>
2016-04-07 08:53:19 +0300

    minor change

--- Bulmaca/settings.py
+++ Bulmaca/settings.py
@@ -120,10 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, "templates"),
-)
-
 STATIC_URL = '/static/'
 
 
